80f47df0f39f639d7d01f1008391931408ff04b6
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:07:25 +0100

    Updated

--- src/components/home/ServiceModel.jsx
+++ src/components/home/ServiceModel.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 const hyps = [
     {
@@ -65,6 +66,16 @@ const ServiceModel = () => {
                                 </div>
                             ))}
                         </div>
+                    </div>
+                    <div className="my-3 flex justify-center">
+                        <Link href="#" className="flex gap-1 font-bold mt-2">
+                            <span className="font-bold underline">Become A Service Incubator </span>
+                            <div>
+                                <svg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.458 11.5H4.51367M17.458 11.5L12.3087 6.35065M17.458 11.5L12.3087 16.6494" stroke="#212121" stroke-width="2" stroke-linecap="square" />
+                                </svg>
+                            </div>
+                        </Link>
                     </div>
                 </div>
             </div>
