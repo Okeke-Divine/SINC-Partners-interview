48d4e102272dc23fdddd77804f1e3a808f5c9c95
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:46:13 +0100

    Updated

--- src/components/home/Blogs.jsx
+++ src/components/home/Blogs.jsx
@@ -3,7 +3,9 @@ const Blogs = () => {
     <>
       <div className="app-padding app-padding-y">
         <h1 className="text-center">Blogs & Resources</h1>
-        <div className="mt-5"></div>
+        <div className="mt-5">
+          
+        </div>
       </div>
     </>
   )
