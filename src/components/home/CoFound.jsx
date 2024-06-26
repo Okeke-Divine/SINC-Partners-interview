d09a874ac25fe6ab6f75814249f446d22dfddc9f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-26 21:53:54 +0100

    Updated

--- src/components/home/CoFound.jsx
+++ src/components/home/CoFound.jsx
@@ -24,6 +24,16 @@ const CoFound = () => {
                         </div>
                     ))}
                 </div>
+                <div className="mt-5 flex justify-center">
+                    <Link href={""} className="flex gap-1 font-bold mt-2">
+                        <span className="font-bold underline">Build your dream</span>
+                        <div>
+                            <svg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.458 11.5H4.51367M17.458 11.5L12.3087 6.35065M17.458 11.5L12.3087 16.6494" stroke="#212121" stroke-width="2" stroke-linecap="square" />
+                            </svg>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </>
     )
