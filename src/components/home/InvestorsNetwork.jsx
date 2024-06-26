e59e0e159a3cb2b2af0e6aa6554f553d5836cfd0
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:09:52 +0100

    Updated

--- src/components/home/InvestorsNetwork.jsx
+++ src/components/home/InvestorsNetwork.jsx
@@ -6,7 +6,7 @@ const InvestorsNetwork = () => {
         <div className="app-flex-center">
           <div className="my-5 text-gray-600 text-center max-w-[900px]">
             Our deals are structured not just to take in investments but to onboard owners passionate about our solutions. Our portfolio companies are valued at $50k at start, with these low valuation, you are guaranteed at least 2x-5x, usually been the first to invest. <br />
-            <span className="mt-1">
+            <span className="mt-2">
               <b>Disclaimer:</b> These deal flows is a statement of our projections and may differ from stage to stage and from venture to venture and the guarantee is for deal 1, usually the first to invest
             </span>
           </div>
