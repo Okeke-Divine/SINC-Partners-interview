fefc954fe3b7266e8b021f2da23ac1a7f4589463
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:47:06 +0100

    Updated

--- src/components/home/Blogs.jsx
+++ src/components/home/Blogs.jsx
@@ -7,13 +7,8 @@ const Blogs = () => {
         <h1 className="text-center">Blogs & Resources</h1>
         <div className="mt-5">
           <div className="flex justify-center">
-            <Link href={network_info.url} className="flex gap-1 font-bold mt-2">
-              <span className="font-bold underline">Learn More</span>
-              <div>
-                <svg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M17.458 11.5H4.51367M17.458 11.5L12.3087 6.35065M17.458 11.5L12.3087 16.6494" stroke="#212121" stroke-width="2" stroke-linecap="square" />
-                </svg>
-              </div>
+            <Link href="#" className="flex gap-1 font-bold mt-2">
+              <span className="font-bold underline">See More Blogs & Resources</span>
             </Link>
           </div>
         </div>
