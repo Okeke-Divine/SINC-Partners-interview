669be17812fd135e2b7e1867406f41e10be31822
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-26 21:56:33 +0100

    Updated

--- src/components/home/CoFound.jsx
+++ src/components/home/CoFound.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
 const infos = [
-    { title: "We Ideate", content: "We internally generate concepts and ideas that help solve problems in our thesis areas, after which we proceed to develop a nano/micro MVP (usually having as low as 30 functions to as high as 100 functions) of the product that we take to the market.", color: "bg-[black]" },
-    { title: "You Validate", content: "You join our 3 months  inResidence program as a business expert to run the operations or as a technical expert to further the development and validate the idea with an average monthly revenue of $1k or 10,000 users and/or an MVP of at least 150 functions", color: "bg-[black]" },
-    { title: "You Co-own", content: "After successful validation and demo day at the end of the EIR program, we move those with perfect fit to become co-founders of the product they validated as CEO & CTO, each owning 20% of the venture.", color: "bg-[black]" },
+    { title: "We Ideate", content: "We internally generate concepts and ideas that help solve problems in our thesis areas, after which we proceed to develop a nano/micro MVP (usually having as low as 30 functions to as high as 100 functions) of the product that we take to the market.", color: "bg-[#303030]" },
+    { title: "You Validate", content: "You join our 3 months  inResidence program as a business expert to run the operations or as a technical expert to further the development and validate the idea with an average monthly revenue of $1k or 10,000 users and/or an MVP of at least 150 functions", color: "bg-[#F47733]" },
+    { title: "You Co-own", content: "After successful validation and demo day at the end of the EIR program, we move those with perfect fit to become co-founders of the product they validated as CEO & CTO, each owning 20% of the venture.", color: "bg-[#FF78BF]" },
 ]
 
 const CoFound = () => {
