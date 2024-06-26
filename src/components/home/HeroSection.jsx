198b3b003a46dd7f9487f72ec47f55d0b7db6395
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:32:40 +0100

    Updated

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,24 +1,74 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
 const HeroSection = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="flex items-center bg-white h-full pl-5 md:pl-10">
-            <div className="max-w-[400px]">
-              <h3>SINC Partners is a service incubation company </h3>
-              <div className="text-gray-600 mt-3">
-                Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).
-              </div>
-              <div className="mt-3">
-                <button className="btn rounded-full text-white app-bg-gray">SINC With Us</button>
+        <Slider {...settings}>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="flex items-center bg-white h-full pl-5 md:pl-10">
+              <div className="max-w-[400px]">
+                <h3>SINC Partners is a service incubation company </h3>
+                <div className="text-gray-600 mt-3">
+                  Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).
+                </div>
+                <div className="mt-3">
+                  <button className="btn rounded-full text-white app-bg-gray">SINC With Us</button>
+                </div>
               </div>
             </div>
+            {/* right */}
+            <div>
+              <img src="/images/hero-image.png" alt="Hero Image" />
+            </div>
           </div>
-          {/* right */}
-          <div>
-            <img src="/images/hero-image.png" alt="Hero Image" />
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="flex items-center bg-white h-full pl-5 md:pl-10">
+              <div className="max-w-[400px]">
+                <h3>SINC Partners is a service incubation company </h3>
+                <div className="text-gray-600 mt-3">
+                  Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).
+                </div>
+                <div className="mt-3">
+                  <button className="btn rounded-full text-white app-bg-gray">SINC With Us</button>
+                </div>
+              </div>
+            </div>
+            {/* right */}
+            <div>
+              <img src="/images/hero-image.png" alt="Hero Image" />
+            </div>
           </div>
-        </div>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="flex items-center bg-white h-full pl-5 md:pl-10">
+              <div className="max-w-[400px]">
+                <h3>SINC Partners is a service incubation company </h3>
+                <div className="text-gray-600 mt-3">
+                  Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).
+                </div>
+                <div className="mt-3">
+                  <button className="btn rounded-full text-white app-bg-gray">SINC With Us</button>
+                </div>
+              </div>
+            </div>
+            {/* right */}
+            <div>
+              <img src="/images/hero-image.png" alt="Hero Image" />
+            </div>
+          </div>
+        </Slider>
       </div>
     </>
   )
