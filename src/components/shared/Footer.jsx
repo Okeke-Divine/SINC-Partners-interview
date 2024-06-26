86161317fc3bf514a012570c18e70c89bd8798be
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-26 22:18:48 +0100

    Updated

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,3 +1,17 @@
+import Link from "next/link"
+
+const platforms = [
+  "Kofoundme",
+  "InResidency",
+  "Service Market",
+  "Founders School",
+  "Metty",
+  "Grantty",
+  "Boldtell",
+  "Chekwa",
+]
+
+
 const Footer = () => {
   return (
     <>
@@ -20,19 +34,65 @@ const Footer = () => {
           {/* links */}
           <div className="custom-px-1">
             <div className="footer">
-              <div className="footer">
+              <div className="footer gap-2 md:gap-5 lg:gap-10">
                 <div>
                   <img src="/images/logo-white.png" className="w-24" alt="logo" />
                   <div>
                     SINC Partners is a service incubation company connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%)
                   </div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                  <div className="font-bold">Platforms</div>
+                  <div className="mt-3 flex flex-col gap-2">
+                    {platforms.map((platform, index) => (
+                      <Link href="#" className="">{platform}</Link>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Initiatives</div>
+                  <div className="mt-3 flex flex-col gap-2">
+                    {platforms.map((platform, index) => (
+                      <Link href="#" className="">{platform}</Link>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">About Us</div>
+                  <div className="mt-3 flex flex-col gap-2">
+                    {platforms.map((platform, index) => (
+                      <Link href="#" className="">{platform}</Link>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">More</div>
+                  <div className="mt-3 flex flex-col gap-2">
+                    {platforms.map((platform, index) => (
+                      <Link href="#" className="">{platform}</Link>
+                    ))}
+                  </div>
+                </div>
               </div>
-              <div></div>
+              {/* locations */}
+              <div>
+                <div className="font-bold">Locations</div>
+                <div className="mt-3">
+                  <div>Abuja, Nigeria </div>
+                  <div>Lagos, Nigeria </div>
+                  <div>Philadelphia, USA</div>
+                </div>
+                <div className="mt-5 footer">
+                  <div>
+                    <img src="/images/aba-white.png" className="w-24" alt="logo" />
+                    <div>Trusted Business</div>
+                  </div>
+                  <div>
+                    <img src="/images/whatsapp.png" className="" alt="logo" />
+                    <div>Chat with US</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="divider"></div>
