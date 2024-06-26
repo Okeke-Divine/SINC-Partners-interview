7365be8e446e0d7fbd0f92d442f2ffb54e925fe0
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:05:31 +0100

    Updated

--- src/components/home/Studio.jsx
+++ src/components/home/Studio.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 const companies = [
   "BILLAAW-1.png",
   "BILLAAW.png",
@@ -67,6 +69,16 @@ const Studio = () => {
                 <img src={`/images/studio/${company}`} alt="company logo" className="object-contain" />
               </div>
             ))}
+          </div>
+          <div>
+            <Link href="#" className="flex gap-1 font-bold mt-2">
+              <span className="font-bold underline">View All Companies </span>
+              <div>
+                <svg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M17.458 11.5H4.51367M17.458 11.5L12.3087 6.35065M17.458 11.5L12.3087 16.6494" stroke="#212121" stroke-width="2" stroke-linecap="square" />
+                </svg>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
