aafa4623d75e6a8b0e5f4d639cfd1219847ad901
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:09:11 +0100

    Updated

--- src/components/home/InvestorsNetwork.jsx
+++ src/components/home/InvestorsNetwork.jsx
@@ -1,6 +1,16 @@
 const InvestorsNetwork = () => {
   return (
-    <div>InvestorsNetwork</div>
+    <>
+      <div className="app-padding app-padding-y">
+        <h1 className="text-center">SINC Investors Network</h1>
+        <div className="app-flex-center">
+          <div className="my-5 text-gray-600 text-center max-w-[1000px]">
+            Our deals are structured not just to take in investments but to onboard owners passionate about our solutions. Our portfolio companies are valued at $50k at start, with these low valuation, you are guaranteed at least 2x-5x, usually been the first to invest. <br />
+            <b>Disclaimer:</b> These deal flows is a statement of our projections and may differ from stage to stage and from venture to venture and the guarantee is for deal 1, usually the first to invest
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
