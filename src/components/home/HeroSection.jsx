ade3ffdcbd79456442d5075ad1f50fa3bc07ffe9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 00:28:09 +0100

    Updated

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -5,12 +5,16 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="max-w-[400px]">
             <h3>SINC Partners is a service incubation company </h3>
-            <div className="text-gray-600 mt-5">
+            <div className="text-gray-600 mt-3">
               Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).
             </div>
-            <div className="mt-5">
+            <div className="mt-3">
               <button className="btn rounded-full text-white app-bg-gray">SINC With Us</button>
             </div>
+          </div>
+          {/* right */}
+          <div>
+
           </div>
         </div>
       </div>
