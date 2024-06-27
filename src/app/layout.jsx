a3cf926d5dfe42c15f7d5122bfe6113992190829
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 02:44:17 +0100

    Updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,12 +5,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "SINC Partners",
+  description: "Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className+" bg-base-200"}>{children}</body>
+      <body className={inter.className + " bg-base-200"}>{children}</body>
     </html>
   );
 }
