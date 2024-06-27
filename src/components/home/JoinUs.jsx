4480d90dee561795a93084eca42e4072bf636a5a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 01:25:46 +0100

    Updated

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -1,3 +1,38 @@
+"use client";
+
+const msgs = [
+    {
+        svg: "",
+        title: "",
+        content: <div>Begin your journey by completing our straightforward application form. Share insights into your entrepreneurial background, expertise, and your vision for supporting the success of our portfolio companies.
+            <br />
+            Our dedicated selection committee, comprised of key stakeholders at SINC Partners, will carefully review your application.
+        </div>
+    },
+    {
+        svg: "",
+        title: "",
+        content: <div>If your application stands out, we'll invite you to an alignment meeting. This is an opportunity to discuss the goals of our EIR program and explore how your expertise aligns with the needs of our portfolio companies.
+            <br />
+            Following the alignment meeting, submit a formal proposal outlining your intended contributions and how you envision adding significant value to our portfolio companies.
+        </div>
+    },
+    {
+        svg: "",
+        title: "",
+        content: <div>Upon successful alignment and proposal review, we'll engage in discussions to finalize the terms of your EIR role. This includes the duration, expectations, and any compensation or equity arrangements.<br />
+            Once terms are agreed upon, we'll draft a formal agreement outlining the specifics of your EIR engagement, including reporting structures, responsibilities, and the support/resources provided by SINC Partners.
+        </div>
+    },
+    {
+        svg: "",
+        title: "",
+        content: <div>Congratulations! As an accepted EIR, you'll undergo an onboarding process to familiarize yourself with the SINC Partners ecosystem, our portfolio companies, and key team members.<br />
+            Facilitated introductions will ensure a seamless integration between you and relevant portfolio companies, setting clear expectations for collaboration.
+        </div>
+    }
+]
+
 const JoinUs = () => {
     return (
         <>
