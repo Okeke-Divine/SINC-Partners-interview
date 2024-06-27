c2797e8b04ca497250e3db5245f776a790d1988d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 02:28:23 +0100

    Updated

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -130,12 +130,12 @@ const JoinUs = () => {
                         </Link>
                     </div>
                     <div className="flex gap-5 justify-center">
-                        <button className="btn rounded-full border-black border-2" onClick={goToPrev}>
+                        <button className="btn rounded-full border-black border-2 w-16 h-16 flex justify-center items-center" onClick={goToPrev}>
                             <svg width="31" height="26" viewBox="0 0 31 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1.94281 13H29.0488M1.94281 13L12.7258 23.783M1.94281 13L12.7258 2.21703" stroke="#4D4D4D" stroke-width="2" stroke-linecap="square" />
                             </svg>
                         </button>
-                        <button className="btn rounded-full border-black border-2" onClick={goToNext}>
+                        <button className="btn rounded-full border-black border-2 w-16 h-16 flex justify-center items-center" onClick={goToNext}>
                             <svg width="31" height="26" viewBox="0 0 31 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M29.0572 13H1.9512M29.0572 13L18.2742 2.21703M29.0572 13L18.2742 23.783" stroke="#4D4D4D" stroke-width="2" stroke-linecap="square" />
                             </svg>
