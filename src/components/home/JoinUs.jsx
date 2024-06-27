4a3cadbf2fd721ebbfad1a9bf0d3c209ed5303cf
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 01:31:43 +0100

    Updated

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -37,6 +37,9 @@ function SlideContent({ icon, title, content }) {
     return (
         <>
             <div className="bg-white app-radius-2 p-5">
+                <div className="flex justify-center items-center bg-black rounded-full w-10 h-10">
+                    <img src={`/images/svg-png/${icon}`} alt="icon" />
+                </div>
                 <div className="font-bold my-2">{title}</div>
                 <div className="text-gray-600 my-2">{content}</div>
             </div>
