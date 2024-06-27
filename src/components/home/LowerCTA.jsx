41d389c4877fba069bb0192496c14c65c7da7899
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 02:14:37 +0100

    Updated

--- src/components/home/LowerCTA.jsx
+++ src/components/home/LowerCTA.jsx
@@ -3,7 +3,7 @@ const LowerCTA = () => {
         <>
 
             <div className="app-padding app-padding-y">
-                <h1 className="text-6xl text-center">Let's build companies that help everyone succeed</h1>
+                <h1 className="text-6xl text-center break-words text-wrap">Let's build companies that help everyone succeed</h1>
                 <div className="mt-5 md:mt-10 flex gap-2 md:gap-5 justify-center">
                     <button className="btn rounded-full border border-black">SINC With Us</button>
                     <button className="btn rounded-full text-white app-bg-gray">Apply to SIP 1.0</button>
