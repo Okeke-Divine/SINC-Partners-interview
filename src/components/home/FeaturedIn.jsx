9b91ae2d02c299de4df4fe2b9273c4e3f417531e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 01:13:33 +0100

    Updated

--- src/components/home/FeaturedIn.jsx
+++ src/components/home/FeaturedIn.jsx
@@ -3,12 +3,23 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-function SlideContent(){
+function SlideContent() {
   return (
     <>
       <div className="flex">
-        <div></div>
-        <div></div>
+        <div className='w-3/4'></div>
+        <div className='w-1/4'>
+          <img src="/featured/punch-newspaper-logo-1024x768.png" alt="news paper logo" />
+          <div className="mt-2 font-bold">SINC Partners invests  over 200 million naira in 5 startups</div>
+          <div className="mt-2 text-gray-600">SINC Partners invests  over 200 million naira in 5 startups. SINC Partners invests  over 200 million naira in 5 startups SINC Partners invests  over 200 million naira in ....</div>
+
+          <div className="mt-10 text-gray-600">
+            REPORTED BY:
+            <br />
+            <br />
+            Rema Viel
+          </div>
+        </div>
       </div>
     </>
   )
@@ -33,7 +44,7 @@ const FeaturedIn = () => {
         <h3 className="text-center">As Featured In</h3>
         <div className="mt-5">
           <Slider {...settings}>
-<SlideContent />
+            <SlideContent />
           </Slider>
         </div>
       </div>
