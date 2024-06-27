d2998395074b0e4068a2a8dbf42428cc304ab596
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 01:50:09 +0100

    Updated

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -14,8 +14,8 @@ const NavBar = () => {
     return (
         <>
             <div className="navbar bg-white px-5 md:px-10 lg:px-16 py-2 md:py-5">
-                <div className="navbar-start flex gap-5">
-                    <img src="/images/logo.png" className="w-24" alt="logo" />
+                <div className="navbar-start flex gap-5 items-center">
+                    <img src="/images/logo.png" className="w-16 md:w-24" alt="logo" />
                     <div className="hidden md:flex gap-2 md:gap-3">
                         {links.map((link, index) => (
                             <Link className="font-bold" href={link.url}>{link.name}</Link>
@@ -23,8 +23,8 @@ const NavBar = () => {
                     </div>
                 </div>
                 <div className="navbar-end gap-2">
-                    <button className="btn rounded-full text-white app-bg-blue">SINC With Us</button>
-                    <button className="btn rounded-full text-white app-bg-gray">Apply to SIP 1.0</button>
+                    <button className="btn-md btn rounded-full text-white app-bg-blue">SINC With Us</button>
+                    <button className="btn-md btn rounded-full text-white app-bg-gray">Apply to SIP 1.0</button>
                 </div>
             </div>
         </>
