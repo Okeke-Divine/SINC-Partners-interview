211235067332e6d5f094e3367220cc0326f57c31
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 02:19:27 +0100

    Updated

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -60,16 +60,30 @@ function SlideContent({ icon, title, content }) {
 
 const JoinUs = () => {
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
-    };
+        const settings = {
+          dots: false,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          pauseOnHover: true,
+          responsive: [
+            {
+              breakpoint: 1024, // Large screens and above
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 768, // Medium screens
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
+        };
 
 
     return (
