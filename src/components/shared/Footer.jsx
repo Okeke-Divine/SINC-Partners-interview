b339c3040c8c1d9fb6d3a04112652d3c242a7138
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 03:49:01 +0100

    Updated

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -67,8 +67,8 @@ const Footer = () => {
             <div className="mt-5">
               <form action="">
                 <div className="border-white rounded-full flex">
-                  <input type="email" placeholder="Enter your email address" required className="input join-item input-bordered bg-transparent border-white rounded-l-full" />
-                  <button className="btn rounded-r-full join-item bg-white text-[#212121]"> Subscribe</button>
+                  <input type="email" placeholder="Enter your email address" required className="input join-item input-bordered bg-transparent border-white rounded-l-full w-full" />
+                  <button className="w-fit btn rounded-r-full join-item bg-white text-[#212121]"> Subscribe</button>
                 </div>
               </form>
             </div>
