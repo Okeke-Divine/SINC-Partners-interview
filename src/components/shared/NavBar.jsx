bd5bb1ecd457a8cd68389f478fa562c9be9ad738
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-27 02:53:24 +0100

    Updated

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
         <>
             <div className="navbar bg-white px-5 md:px-10 lg:px-16 py-2 md:py-5 items-center">
                 <div className="navbar-start flex gap-5 items-center">
-                    <img src="/images/logo.png" className="w-16 md:w-24" alt="logo" />
+                    <img src="/images/logo.png" className="w-20 md:w-24" alt="logo" />
                     <div className="hidden md:flex gap-2 md:gap-3">
                         {links.map((link, index) => (
                             <Link className="font-bold" href={link.url}>{link.name}</Link>
